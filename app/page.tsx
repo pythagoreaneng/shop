f6e3310190c46bb0320c475887d7531b664cf54d
Katsuya Wakabayashi <wakabayashi@ucla.edu>
2023-09-06 23:26:51 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { EB_Garamond } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const mainFont = EB_Garamond({ subsets: ["latin"], weight: "400" });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${mainFont.className}`}
+    >
+      <header className="text-4xl m-10">Pythagorean</header>
+      <h2 className="h-screen">
+        <Image
+          src="/product.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </h2>
+      <div className="flex flex-col">
+        <div className="flex space-x-7 justify-center m-4">$149</div>
+        <div className="flex space-x-7 justify-center ">
+          <div>S</div>
+          <div>M</div>
+          <div>L</div>
+          <div>XL</div>
+        </div>
+      </div>
+    </main>
+  );
+}
