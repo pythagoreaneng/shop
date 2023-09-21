914c991a405d09d67657db585833f59c18a1e57e
Nasser Ben <nasserben@Nassers-MacBook-Air.local>
2023-09-21 16:49:31 -0700

    add stripe api

---
+++ server.js
@@ -0,0 +1,19 @@
+// require("dotenv").config();
+
+// // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const express = require("express");
+// const app = express();
+// const port = 3000; // Choose a port of your choice
+// app.use(express.json());
+
+// app.post("/", async (req, res) => {
+//   try {
+//     res.redirect(303, session.url);
+//   } catch (err) {
+//     res.status(err.statusCode || 500).json(err.message);
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
